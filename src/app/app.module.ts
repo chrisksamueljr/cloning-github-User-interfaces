c01a60336f1bb827ea6c848ef0837584b89872be
Chris Samuel <ksamuel.chris@gmail.com>
2019-04-08 09:26:55 -0400

    created the building blocks for developing API driven apps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { RepoComponent } from './repo/repo.component';
+import { TopReposComponent } from './top-repos/top-repos.component';
+
+
+
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    RepoComponent,
+    TopReposComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    FormsModule,
+    NgbModule.forRoot(),
+
+    SharedModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/top-repos/top-repos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-repo',
+  templateUrl: './top-repo.component.html',
+  styleUrls: ['./top-repo.component.css']
+})
+export class TopReposComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
