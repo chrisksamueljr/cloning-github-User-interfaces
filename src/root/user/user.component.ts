4cb5214bb3a1f1861cd66d6db423fa1984333a1a
Chris Samuel <ksamuel.chris@gmail.com>
2019-04-11 16:00:30 -0400

    setup the mockup for the UserComponent

--- src/root/user/user.component.ts
+++ src/root/user/user.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from './user.types';
+import { GithubApiService } from '../core/github-api.service';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-user',
+  selector: 'user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  // User
+  user: User;
+
+  constructor(
+    private ghas: GithubApiService,
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    this.route.params.pipe(switchMap(() => this.ghas.getUser(params['login'])))
+    .subscribe(user => this.user = user);
   }
 
 }
