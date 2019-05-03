bb9488f21dee09cb29db669ead1b372e89ee61f3
Chris Samuel <chrisk.samueljr@gmail.com>
2019-05-03 13:32:24 -0400

    Module was in multiple shared Modules

--- src/root/shared/shared.module.ts
+++ src/root/shared/shared.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {LargeNumberShortenPipe} from  './large-number-shorten.pipe';
-import { RepositorySearchInputComponent } from './repository-search-input/repository-search-input.component';
+// import { RepositorySearchInputComponent } from './repository-search-input/repository-search-input.component';
 // import { EventCardViewComponent } from './event-card-view/event-card-view.component';
 
 
 @NgModule({
   declarations: [LargeNumberShortenPipe, 
-    RepositorySearchInputComponent, 
+    // RepositorySearchInputComponent, 
     // EventCardViewComponent
   ],
   imports: [
