d94425b10761a60a12f15b45f64d6c1b3a944af5
Chris Samuel <chrisk.samueljr@gmail.com>
2019-05-06 16:27:27 -0400

    created the function for the loading the repoReadme OnInit logging to screen

--- src/root/repo/repo-readme/repo-readme.component.ts
+++ src/root/repo/repo-readme/repo-readme.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { switchMap } from 'rxjs/operators';
+import {GithubApiService} from '../../core/github-api.service';
 
 @Component({
   selector: 'repo-readme',
@@ -7,9 +12,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RepoReadmeComponent implements OnInit {
 
-  constructor() { }
+	repoReadme: string
 
-  ngOnInit() {
+  
+  constructor (
+    private ghas: GithubApiService,
+    private activatedRoute: ActivatedRoute,
+    private location: Location
+  ) { 
+
+    
+  }
+ngOnInit() {  
+
+// this.activatedRoute.params.subscribe(data => console.log(data)); // {login: "user-name"}
+    this.activatedRoute.params.pipe(switchMap((params: Params) => 
+    // console.log(params)
+    this.ghas.getRepoReadme(params['owner'], params['name'])))
+    // .subscribe(repos => console.log(repos));
+    .subscribe(repoReadme => this.repoReadme = repoReadme );
+  
   }
 
 }
